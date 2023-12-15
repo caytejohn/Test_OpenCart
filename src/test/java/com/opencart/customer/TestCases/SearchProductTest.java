48caa6b3624c833cd0820eb976f22ec3c87e84f9
CJ <79216259+caytejohn@users.noreply.github.com>
2023-12-15 15:43:27 +0800

    Create test for product search

--- src/test/java/com/opencart/customer/TestCases/SearchProductTest.java
+++ src/test/java/com/opencart/customer/TestCases/SearchProductTest.java
@@ -1,2 +1,35 @@
-package com.opencart.customer.TestCases;public class SearchProductTest {
+package com.opencart.customer.TestCases;
+
+import com.opencart.customer.PageObjects.HomePage;
+import com.opencart.customer.PageObjects.SearchPage;
+import com.opencart.customer.TestBase.BaseClass;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SearchProductTest extends BaseClass {
+
+    @Test
+    public void test_search_item () {
+        logger.info("Starting testing for search item");
+
+        try {
+            HomePage home = new HomePage(driver);
+            SearchPage search = new SearchPage(driver);
+
+            home.searchItems(resBundle.getString("item"));
+            logger.info("Input item name on search box");
+            home.clickSearchBtn();
+            logger.info("Clicked search button");
+
+            search.isProductExist(resBundle.getString("item"));
+
+            logger.info("Validating if the item exists");
+            Assert.assertTrue(search.isProductExist(resBundle.getString("item")));
+        }
+        catch (Exception e) {
+
+            Assert.fail();
+        }
+        logger.info("Finished testing for search item");
+    }
 }
