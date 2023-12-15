370fc2073b4c80107ffe2fa0f8718a36dec49199
CJ <79216259+caytejohn@users.noreply.github.com>
2023-12-15 15:40:34 +0800

    Create actions and add webelements for search page

--- src/test/java/com/opencart/customer/PageObjects/SearchPage.java
+++ src/test/java/com/opencart/customer/PageObjects/SearchPage.java
@@ -1,2 +1,67 @@
-package com.opencart.customer.PageObjects;public class SearchPage {
+package com.opencart.customer.PageObjects;
+
+import com.opencart.customer.PageObjects.Base.BasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.util.List;
+
+public class SearchPage extends BasePage {
+
+    public SearchPage(WebDriver driver) {
+        super(driver);
+    }
+
+    // WebElements
+    @FindBy(xpath = "//*[@id='content']//img")
+    List<WebElement> resultProducts;
+
+    @FindBy(id = "input-quantity")
+    WebElement txtQuantity;
+
+    @FindBy(xpath = "//button[@id='button-cart']")
+    WebElement btnAddToCart;
+
+    @FindBy(xpath = "//div[@class='alert alert-success alert-dismissible']")
+    WebElement msgSuccessAddToCart;
+
+    // Action Methods
+    public boolean isProductExist(String productName) {
+        boolean flag = false;
+        for(WebElement product:resultProducts) {
+            if(product.getAttribute("title").equalsIgnoreCase(productName)) {
+                flag = true;
+                break;
+            }
+        }
+        return flag;
+    }
+
+    public void selectItem(String itemName) {
+        for (WebElement product : resultProducts) {
+            if (product.getAttribute("title").equals(itemName)) {
+                wait.until(ExpectedConditions.visibilityOf(product)).click();
+            }
+        }
+    }
+
+    public void setQuantity(String quantity) {
+        wait.until(ExpectedConditions.visibilityOf(txtQuantity)).clear();
+        txtQuantity.sendKeys(quantity);
+    }
+
+    public void clickAddToCartBtn() {
+        btnAddToCart.click();
+    }
+
+    public boolean isMsgSuccessAddToCartIsDisplayed () {
+        try {
+            return msgSuccessAddToCart.isDisplayed();
+        }
+        catch (Exception e) {
+            return false;
+        }
+    }
 }
