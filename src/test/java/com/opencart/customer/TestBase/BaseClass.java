c58eb5e4835f45bc4e72301ed57306695e9369bc
CJ <79216259+caytejohn@users.noreply.github.com>
2023-12-22 16:05:48 +0800

    Fix drovers

--- src/test/java/com/opencart/customer/TestBase/BaseClass.java
+++ src/test/java/com/opencart/customer/TestBase/BaseClass.java
@@ -2,18 +2,17 @@ package com.opencart.customer.TestBase;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.*;
 
 import java.util.ResourceBundle;
 
 public class BaseClass extends CommonMethods {
 
-    public WebDriver driver;
+    public RemoteWebDriver driver;
     public Logger logger;
     public ResourceBundle resBundle;
 
@@ -25,14 +24,6 @@ public class BaseClass extends CommonMethods {
 
         switch (browser) {
             case "chrome" -> {
-                /* ChromeOptions options = new ChromeOptions();
-                options.addArguments("--incognito ");
-                options.addArguments("--headless=new");
-                options.addArguments("--allow-insecure-localhost");
-                options.addArguments("--disable-gpu");
-                options.addArguments("--window-size=1280,800");
-                options.setCapability("acceptInsecureCerts", true);
-                 */
                 driver = new ChromeDriver();
                 logger.info("Opening the Chrome browser");
             }
